78371c8d07bc6fd700d58515f4f6b8eac8466382
Mallne <info@mallne.cloud>
2026-07-13 20:23:38 +0200

    Upgrade dependencies and refine Gradle scripts
    Updated several dependencies including Kotlin, Ktor, and Maven Publish to newer versions. Improved Gradle scripts by replacing `properties` with `project.findProperty` for enhanced property resolution. Adjusted Gradle wrapper configurations and fixed syntax for better consistency in source set declarations.

--- plugins/adapter-xml/src/commonMain/kotlin/cloud/mallne/dicentra/aviator/plugin/adapter/xml/XmlAdapter.kt
+++ plugins/adapter-xml/src/commonMain/kotlin/cloud/mallne/dicentra/aviator/plugin/adapter/xml/XmlAdapter.kt
@@ -4,7 +4,7 @@ import cloud.mallne.dicentra.aviator.core.AviatorServiceDataHolder
 import cloud.mallne.dicentra.aviator.core.plugins.AviatorPlugin
 import cloud.mallne.dicentra.aviator.core.plugins.AviatorPluginInstance
 import kotlinx.serialization.*
-import nl.adaptivity.xmlutil.dom2.Element
+import nl.adaptivity.xmlutil.dom2.*
 import nl.adaptivity.xmlutil.serialization.XML
 
 object XmlAdapter : AviatorPlugin<XmlAdapterPluginConfig> {
@@ -21,7 +21,9 @@ object XmlAdapter : AviatorPlugin<XmlAdapterPluginConfig> {
     fun XML.parseToElement(content: String): Element = decodeFromString(content)
     fun <T> XML.encodeToElement(serializer: SerializationStrategy<T>, value: T): Element {
         val stringified: String = encodeToString(serializer, value)
-        return decodeFromString(stringified)
+        val element: Element = decodeFromString(stringified)
+        element.stripNamespaceDeclarations()
+        return element
     }
 
     inline fun <reified T> XML.encodeToElement(value: T): Element =
@@ -33,5 +35,25 @@ object XmlAdapter : AviatorPlugin<XmlAdapterPluginConfig> {
     fun <T> XML.decodeFromElement(deserializer: DeserializationStrategy<T>, element: Element): T {
         val serialized = encodeToString(element)
         return decodeFromString(deserializer, serialized)
+    }
+}
+
+internal fun Element.stripNamespaceDeclarations() {
+    val attrs = attributes
+    val nsAttrs = mutableListOf<Attr>()
+    for (attr in attrs) {
+        if (attr.name.startsWith("xmlns")) {
+            nsAttrs.add(attr)
+        }
+    }
+    for (nsAttr in nsAttrs) {
+        removeAttributeNode(nsAttr)
+    }
+    var child: Node? = firstChild
+    while (child != null) {
+        if (child is Element) {
+            child.stripNamespaceDeclarations()
+        }
+        child = child.nextSibling
     }
 }
