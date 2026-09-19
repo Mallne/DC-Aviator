42991b65284e61a54062db77df5be68853c61133
mallne <info@mallne.cloud>
2026-09-19 10:21:47 +0200

    fix: safe credential casting for Gradle 9.x

--- clients/mock/build.gradle.kts
+++ clients/mock/build.gradle.kts
@@ -38,8 +38,8 @@ publishing {
                 maven {
                     url = uri("https://registry.mallne.cloud/repository/DiCentraArtefacts/")
                     credentials {
-                        username = project.findProperty("dc.username") as String?
-                        password = project.findProperty("dc.password") as String?
+                        username = project.findProperty("dc.username") as? String ?: ""
+                        password = project.findProperty("dc.password") as? String ?: ""
                     }
                 }
             }
